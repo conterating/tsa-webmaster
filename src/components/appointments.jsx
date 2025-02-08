00ef77780f2a840af68e73b413d3d88890de05cf
ryder <ryderortiz7@gmail.com>
2025-02-08 11:05:06 -0500

    Create appointments.jsx

---
+++ src/components/appointments.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Appointments.module.css";
+
+const Appointments = () => {
+  return (
+    <section className={styles.appointmentsSection}>
+      <h2>Book an Appointment</h2>
+      <p>Schedule your dine-in experience with us or order for carry-out.</p>
+    </section>
+  );
+};
+
+export default Appointments;
